e0e01d075ab58117f2aca5df3fb7e992319adf70
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-24 18:49:04 -0600

    fixed account set to null bug

--- client/src/models/Account.js
+++ client/src/models/Account.js
@@ -1,7 +1,7 @@
 import { Profile } from "./Profile";
 
 export class Account extends Profile {
-  constructor({ data}) {
+  constructor(data) {
     super(data)
     this.email = data.email;
     // TODO add additional properties if needed
